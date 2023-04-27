76310e3dd936650f678ae4ec73f733afd3c725a4
roydunn <roydunn100@gmail.com>
2023-04-27 23:01:02 +0000

    removed

--- client/app/components/CreateScoutingReport/CreateScoutingReportDialog.tsx
+++ client/app/components/CreateScoutingReport/CreateScoutingReportDialog.tsx
@@ -13,7 +13,6 @@ const CreateScoutingReportDialog = () => {
 
   return (
     <>
-      {/* dsf */}
       <Button
         variant="contained"
         style={{ height: '32px', marginRight: '2.2em' }}
