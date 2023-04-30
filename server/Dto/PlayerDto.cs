7c3f17f401762eed29416fcbd6b75388c08dde2b
Ben Fristad <61523476+bfristad@users.noreply.github.com>
2023-04-29 20:25:05 -0700

    Update server/Dto/PlayerDto.cs
    Co-authored-by: Roy Dunn <63774178+roydunn19@users.noreply.github.com>

--- server/Dto/PlayerDto.cs
+++ server/Dto/PlayerDto.cs
@@ -5,7 +5,6 @@ public class PlayerDto
     public int number { get; set; }
     public string name { get; set; }
     public int pts { get; set; }
-
     public double fgm { get; set; }
     public double fga { get; set; }
     public double fgp { get; set; }
