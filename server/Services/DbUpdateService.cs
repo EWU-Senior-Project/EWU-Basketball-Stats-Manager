54ec76f472f05b8584dd80b4f86c6fb22d762d82
mpennock5 <mpennock6@outlook.com>
2023-05-16 21:24:17 -0700

    DbUpdateService work

--- server/Services/DbUpdateService.cs
+++ server/Services/DbUpdateService.cs
@@ -26,6 +26,61 @@ public class DbUpdateService
             var filteredRecords = verboseRecords.GroupBy(team => team.TeamId)
                                                 .Select(group => group.First())
                                                 .ToList();
+
+            foreach (var row in filteredRecords)
+            {
+                if(!context.Teams.Any(team => team.TeamId == row.TeamId))
+                {
+                    context.Teams.Add(row);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+
+    public static void SeedPlayers (AppDbContext context)
+    {
+        using (var reader = new StreamReader("../Content/player_box.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<PlayerClassMap>();
+            var verboseRecords = csv.GetRecords<Player>();
+
+            var filteredRecords = verboseRecords.GroupBy(player => player.PlayerId)
+                                                .Select(group => group.First())
+                                                .ToList();
+
+            foreach (var row in filteredRecords)
+            {
+                if(!context.Players.Any(player => player.PlayerId == row.PlayerId))
+                {
+                    context.Players.Add(row);
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+
+    public static void SeedGames (AppDbContext context)
+    {
+        using (var reader = new StreamReader("../Content/player_box.csv"))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<GameClassMap>();
+            var verboseRecords = csv.GetRecords<Game>();
+
+            var filteredRecords = verboseRecords.GroupBy(game => game.GameId)
+                                                .Select(group => group.First())
+                                                .ToList();
+
+            foreach (var row in filteredRecords)
+            {
+                if(!context.Games.Any(game => game.GameId == row.GameId))
+                {
+                    context.Games.Add(row);
+                }
+            }
+            context.SaveChanges();
         }
     }
 }
